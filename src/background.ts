39b97e0368dfb2cca862c19cb6e2ae99ad570b53
Bart <bartoszkocmierowski@gmail.com>
2024-12-12 13:04:23 +0100

    changes in background

--- src/background.ts
+++ src/background.ts
@@ -2,13 +2,30 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color: '#4320f5' });
   });
 
+  let activeTabId: any = null;
+
   chrome.webNavigation.onCompleted.addListener((details) => {
     if (details.url.includes("https://dashboard.ctnbee.com/orders/order-items-to-be-prepared/*")) {
       chrome.scripting.executeScript({
         target: { tabId: details.tabId },
         files: ["content.js"]
       });
+      activeTabId = details.tabId;
     }
   }, { url: [{ urlContains: "https://dashboard.ctnbee.com/orders/order-items-to-be-prepared/*" }] });
 
+  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    if (details.tabId === activeTabId) {
+      activeTabId = null; // Reset activeTabId on navigation
+    }
+  });
+
   console.log("message from background")
+
+  chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    let adress = details.url.includes("https://dashboard.ctnbee.com/orders/order-items-to-be-prepared/");
+    console.log(adress)
+    // if (details.url.includes("https://dashboard.ctnbee.com/orders/order-items-to-be-prepared/*)) {
+    //   console.log("works")
+    // }
+  }, { url: [{ urlMatches: "https://dashboard.ctnbee.com" }] });
